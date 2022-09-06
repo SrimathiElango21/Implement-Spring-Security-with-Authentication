e9fcde1a3f7049f6b4121cbb65fe0c3922511f61
Srimathi Elango <86002860+SrimathiElango21@users.noreply.github.com>
2022-09-06 16:22:27 +0530

    Phase3 Practice project4

---
+++ Spring Security/src/main/java/com/example/demo/GreetController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class GreetController {
+	@GetMapping("/")
+	public String getMessage() {
+		return "<h1> Hello All</h1>";
+	}
+
+}
---
+++ Spring Security/src/main/java/com/example/demo/SpringPhase3PracticeProject4Application.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPhase3PracticeProject4Application  {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPhase3PracticeProject4Application.class, args);
+	}
+}
